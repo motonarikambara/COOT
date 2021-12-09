fdc62f1a34533ac2ec9e7f4cd1319f2a75f18e87
motonarikambara <motonari.k714@gmail.com>
2021-12-09 18:15:13 +0900

    val用のjsonファイル作成スクリプトを追加

---
+++ annotations/ponnet/make_val_para.py
@@ -0,0 +1,10 @@
+import json
+
+with open("captioning_val.json", "r") as f:
+    cap_data = json.load(f)
+val_dict = {}
+for i in cap_data:
+    val_dict[i["clip_id"]] = i["sentence"]
+
+with open('captioning_val_para', 'w') as f:
+    json.dump(val_dict, f)
